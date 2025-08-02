e98366b54ede99b83158167a687bad86a6d67292
TsanChao <TsanChao@users.noreply.github.com>
2025-08-03 00:51:22 +0800

    Add synergy analyzer

---
+++ core/synergy_analyzer.py
@@ -0,0 +1,15 @@
+# 战法协同分析器
+
+class SynergyAnalyzer:
+    def __init__(self, data_manager):
+        self.data_manager = data_manager
+    
+    def analyze_synergy(self, team_heroes):
+        """分析队伍中武将的协同效应"""
+        # 实现协同分析逻辑
+        pass
+    
+    def calculate_synergy_score(self, hero_team):
+        """计算队伍协同评分"""
+        # 实现协同评分计算
+        pass
